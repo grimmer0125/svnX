f7c8a848113a64755cd7cc5a460647fea50d5cb9
dom <dom@0e40938a-cd37-11dd-aa7b-9fdd872f0417>
2006-05-27 14:47:04 +0000

    Applied patch from ticket #39 : Support for BBEdit diff.

--- trunk/svndiff.sh
+++ trunk/svndiff.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Uncomment exactly one of the following 4 lines to set the diff app
+#appToDoDiffWith="$SVNX_FILEMERGE_DIFF"
+#appToDoDiffWith="$SVNX_CODEWARRIOR_DIFF"
+#appToDoDiffWith="$SVNX_TEXTWRANGLER_DIFF"
+#appToDoDiffWith="$SVNX_BBEDIT_DIFF"
+
 #echo $@
 #until [ -z "$1" ]
 #do
@@ -32,45 +38,43 @@ cp -f "$7" "$secondTempFile"
 f=`echo "$7" | sed -e 's/\.svn\/tmp\/\(.*\)\.tmp$/\1/'`
 
 name=$5
-isWorkingCopy=${name/*(working copy)/1}
+workingCopyFlag=${name/*(working copy)/1}
 
-# Uncomment exactly one of these 3 lines
-
-#appToDoDiffWith="codewarrior"
-#appToDoDiffWith="opendiff"
-#appToDoDiffWith="textwrangler"
-
-if [ "$isWorkingCopy" == "1" ]
+WORKING_COPY="1"
+firstFile=$firstTempFile
+if [ "$workingCopyFlag" == "$WORKING_COPY" ]
 then
-	if [ "$appToDoDiffWith" == "codewarrior" ]
-	then
-		osascript -e \
-		"tell application \"CodeWarrior IDE\" 
-		    activate
-		    set fileOne to POSIX file \"$firstTempFile\" 
-		    set fileTwo to POSIX file \"$f\" 
-		    Compare Files fileOne to fileTwo with case sensitive and ignore extra space
-		end tell"
-	elif [ "$appToDoDiffWith" == "textwrangler" ]
-	then
-		/usr/bin/twdiff --case-sensitive "$firstTempFile" "$f"
-	else #elif [ "$appToDoDiffWith" == "opendiff" ]
-		/usr/bin/opendiff "$firstTempFile" "$f" -merge "$f" &> /dev/null
-	fi
+	secondFile=$f ; 
+	isWorkingCopy=true
 else
-	if [ "$appToDoDiffWith" == "codewarrior" ]
-	then
-		osascript -e \
-		"tell application \"CodeWarrior IDE\" 
-		    activate
-		    set fileOne to POSIX file \"$firstTempFile\" 
-		    set fileTwo to POSIX file \"$secondTempFile\" 
-		    Compare Files fileOne to fileTwo with case sensitive and ignore extra space
-		end tell"
-	elif [ "$appToDoDiffWith" == "textwrangler" ]
-	then
-		/usr/bin/twdiff --case-sensitive "$firstTempFile" "$secondTempFile"
-	else #elif [ "$appToDoDiffWith" == "opendiff" ]
-		/usr/bin/opendiff "$firstTempFile" "$secondTempFile" &> /dev/null
-	fi
+	secondFile=$secondTempFile
 fi
+
+codewarrior_diff()
+{
+	osascript -e \
+	"tell application \"CodeWarrior IDE\" 
+		activate
+		set fileOne to POSIX file \"$1\" 
+		set fileTwo to POSIX file \"$2\" 
+		Compare Files fileOne to fileTwo with case sensitive and ignore extra space
+	end tell"
+}
+
+case "$appToDoDiffWith" in
+	"$SVNX_CODEWARRIOR_DIFF" ) codewarrior_diff $firstFile $secondFile ;;
+	"$SVNX_TEXTWRANGLER_DIFF" ) /usr/bin/twdiff --case-sensitive "$firstFile" "$secondFile" ;;
+	"$SVNX_BBEDIT_DIFF" ) /usr/bin/bbdiff --case-sensitive "$firstFile" "$secondFile" ;;
+
+	# $SVNX_FILEMERGE_DIFF | * is kind of redundant, as * matches $SVNX_FILEMERGE_DIFF
+	# as well, but it makes the code more explicit
+
+	"$SVNX_FILEMERGE_DIFF" | * ) 
+		if [ $isWorkingCopy ]
+		then 
+			/usr/bin/opendiff "$firstFile" "$secondFile" -merge "$secondFile" &> /dev/null
+		else 
+			/usr/bin/opendiff "$firstFile" "$secondFile"
+		fi
+		;;
+esac

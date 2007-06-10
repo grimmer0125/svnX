80902c27a0f8873cf31f93b3231eda722a549650
dom <dom@0e40938a-cd37-11dd-aa7b-9fdd872f0417>
2007-06-10 11:41:23 +0000

    - New bullet-proof parsing method for svn status that uses --xml output. Requires a subversion 1.4 client. Can be disabled in the preferences. - Added a tooltip that explains the meaning of the columns' characters. - Added a thin vertical separator in working copy browser.

--- TableViewDelegate.m
+++ TableViewDelegate.m
@@ -45,6 +45,109 @@
 	return FALSE;
 }
 
+- (NSString *)tableView:(NSTableView *)aTableView toolTipForCell:(NSCell *)aCell rect:(NSRectPointer)rect tableColumn:(NSTableColumn *)aTableColumn row:(int)row mouseLocation:(NSPoint)mouseLocation
+{
+	//[super tableView:aTableView toolTipForCell:aCell rect:rect tableColumn:aTableColumn row:row mouseLocation:mouseLocation];
+	NSDictionary *rowDict = [[svnFilesAC arrangedObjects] objectAtIndex:row];
+	NSDictionary *comments;
+	
+	switch ( [[aTableColumn identifier] intValue] )
+	{
+		case 1:
+			
+			comments = [NSDictionary dictionaryWithObjectsAndKeys:
+			@"No modifications.", @" ", 
+			@"Item is scheduled for Addition.", @"A", 
+			@"Item is scheduled for Deletion.", @"D", 
+			@"Item has been modified.", @"M", 
+			@"Item has been replaced in your working copy. This means the file was scheduled for deletion, and then a new file with the same name was scheduled for addition in its place.", @"R", 
+			@"The contents (as opposed to the properties) of the item conflict with updates received from the repository.", @"C", 
+			@"Item is related to an externals definition.", @"X", 
+			@"Item is being ignored (e.g. with the svn:ignore property).", @"I", 
+			@"Item is not under version control.", @"?", 
+			@"Item is missing (e.g. you moved or deleted it without using svn). This also indicates that a directory is incomplete (a checkout or update was interrupted).", @"!", 
+			@"Item is versioned as one kind of object (file, directory, link), but has been replaced by different kind of object.", @"~", 
+			nil];
+			return [comments objectForKey:[rowDict objectForKey:@"col1"]];
+			
+		break;
+
+		case 2:
+			
+			comments = [NSDictionary dictionaryWithObjectsAndKeys:
+			@"No properties modifications.", @" ", 
+			@"Properties for this item have been modified.", @"M",
+			@"Properties for this item are in conflict with property updates received from the repository.", @"C",
+			nil];
+			return [comments objectForKey:[rowDict objectForKey:@"col2"]];
+			
+		break;
+
+		case 3:
+			
+			comments = [NSDictionary dictionaryWithObjectsAndKeys:
+			@"Item is not locked.", @" ", 
+			@"Item is locked. (You may want to run svn clean up!)", @"L",
+			nil];
+			return [comments objectForKey:[rowDict objectForKey:@"col3"]];
+			
+		break;
+
+		case 4:
+			
+			comments = [NSDictionary dictionaryWithObjectsAndKeys:
+			@"No history scheduled with commit.", @" ", 
+			@"History scheduled with commit.", @"+",
+			nil];
+			return [comments objectForKey:[rowDict objectForKey:@"col4"]];
+			
+		break;
+
+		case 5:
+			
+			comments = [NSDictionary dictionaryWithObjectsAndKeys:
+			@"Item is a child of its parent directory.", @" ", 
+			@"Item is switched.", @"S",
+			nil];
+			return [comments objectForKey:[rowDict objectForKey:@"col5"]];
+			
+		break;
+
+		case 6:
+			
+			comments = [NSDictionary dictionaryWithObjectsAndKeys:
+			@"File is not locked in this working copy.", @" ", 
+			@"File is locked in this working copy.", @"K",
+			@"File is locked either by another user or in another working copy.", @"O",
+			@"File was locked in this working copy, but the lock has been “stolen” and is invalid. The file is currently locked in the repository.", @"T",
+			@"File was locked in this working copy, but the lock has been “broken” and is invalid. The file is no longer locked.", @"B",
+			nil];
+			return [comments objectForKey:[rowDict objectForKey:@"col6"]];
+			
+		break;
+
+		case 7:
+			
+			comments = [NSDictionary dictionaryWithObjectsAndKeys:
+			@"Item is up-to-date.", @"*",
+			@"A newer revision of the item exists on the server.", @"*",
+			nil];
+			return [comments objectForKey:[rowDict objectForKey:@"col7"]];
+			
+		break;
+
+		case 8:
+			
+			comments = [NSDictionary dictionaryWithObjectsAndKeys:
+			@"File has properties", @"P",
+			@"File doesn't have any property", @" ",
+			nil];
+			return [comments objectForKey:[rowDict objectForKey:@"col8"]];
+			
+		break;
+	}
+	return @"";
+}
 
 -(id)document
 {
